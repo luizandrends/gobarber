5fc03aaf0deaf86890edf513af51c5651869fb8e
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2019-10-16 01:32:56 -0300

    chore: backend first commit

---
+++ backend/src/config/mail.js
@@ -0,0 +1,11 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '5f7fdd00dacb0c',
+    pass: 'c9548cbe0525ae',
+  },
+  default: {
+    from: 'Equipe gobarber <noreply@gobarber.com>',
+  },
+};
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://59eb85c6cd844e8886a76e2ab03661a6@sentry.io/1516466',
+};
